82ce61ad4f914eb724252297558e1a98ac388814
Andreas Fink <andreas@andreasfink.xyz>
2020-04-05 18:20:43 +0200

    add api

---
+++ src/php/sinusbot/autoload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Entrypoint Autoload | src/autoload.php
+ *
+ * Autoloader to use the SinusBot PHP Library
+ *
+ * @package      SinusBot
+ * @author       Max Schmitt <max@schmitt.mx>
+ */
+
+include_once("RestClient.php");
+include_once("Instance.php");
+include_once("Playlist.php");
+include_once("User.php");
+include_once("API.php");
