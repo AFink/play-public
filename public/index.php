bcbeab0c192dc2ad7dd1eb766ee79853ebe3244a
Johannes Krüger <web@johanneskr.de>
2020-04-19 21:41:46 +0200

    10+ Radio station added

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ include("../includes/head.php");
         <!-- Sidebar  -->
         <nav id="sidebar">
             <div class="sidebar-header">
-                <h3>Bootstrap Sidebar</h3>
+                <h3>Public-Instance-Webinterface</h3>
             </div>
 
             <ul class="list-unstyled components">
@@ -19,17 +19,6 @@ include("../includes/head.php");
                 <li id="youtube" onclick="showYoutube()"><a>Youtube</a></li>
                 <li id="radio" onclick="showRadio()"><a>Radio Stations</a></li>
             </ul>
-
-            <ul id="playlists" class="list-unstyled components">
-            </ul>
-                <ul class="list-unstyled CTAs">
-                    <li>
-                        <a href="https://bootstrapious.com/tutorial/files/sidebar.zip" class="download">Download source</a>
-                    </li>
-                    <li>
-                        <a href="https://bootstrapious.com/p/bootstrap-sidebar" class="article">Back to article</a>
-                    </li>
-                </ul>
         </nav>
 
         <!-- Page Content  -->
@@ -59,7 +48,7 @@ include("../includes/head.php");
                     <thead>
                         <tr>
                             <th>Name</th>
-                            <th>Type</th>
+                            <th>Description</th>
                         </tr>
                     </thead>
                     <tbody class="tbody-radio">
