a5a16c1b0df157fb01b2af469880b2a2f471b2fc
Andreas <andreas@andreasfink.xyz>
2020-05-03 22:49:52 +0200

    :)

--- includes/footer.php
+++ includes/footer.php
@@ -3,7 +3,6 @@
 <script src="<?php echo _ROOTURL ?>src/js/bootstrap.min.js"></script>
 <script src="<?php echo _ROOTURL ?>src/js/rangeSlider.min.js"></script>
 <script src="<?php echo _ROOTURL ?>src/js/sweetalert2.min.js"></script>
-      <link rel="stylesheet" href="<?php echo _ROOTURL ?>src/css/sweetalert2.min.css">
 <script src="<?php echo _ROOTURL ?>src/js/datatables.min.js"></script>
 <script src="<?php echo _ROOTURL ?>src/js/mCustomScrollbar.min.js"></script>
 <script src="<?php echo _ROOTURL ?>src/js/darkmode.js"></script>
