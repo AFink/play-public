351787a604d9f5fd726b2b25345c454257e0e03f
Andreas <andreas@andreasfink.xyz>
2020-05-03 10:35:29 +0200

    update lang

--- includes/language.php
+++ includes/language.php
@@ -1,6 +1,8 @@
 <?php
 class Language {
 
+  private $path = '../langs/';
+  private $extention = '.json';
   private $UserLng;
   private $langSelected;
   private $lang;
@@ -8,23 +10,25 @@ class Language {
   private $langs;
 
 
-  public function __construct(){
+  public function __construct($path = null, $extention = null){
+    if ($path !== null) {
+      $this->path = $path;
+    }
+    if ($extention !== null) {
+      $this->extention = $extention;
+    }
       if (isset($_GET["lang"])) {
         $lang = $_GET["lang"];
-        echo $lang;
-      }elseif (isset($_SESSION["lang"])) {
-        $lang = $_SESSION["lang"];
+      }elseif (isset($_COOKIE["lang"])) {
+        $lang = $_COOKIE["lang"];
       }else {
         $lang = null;
       }
-      echo $lang;
-      $this->UserLng = $this->detectLang($this->getExistingLangs(), 'de', $lang, false);
-      echo $this->UserLng;
-      $_SESSION["lang"] = $this->UserLng;
-      //construct lang file
-      $langFile = '../langs/'. $this->UserLng . '.json';
+      $this->UserLng = $this->detectLang($this->getExistingLangs(), 'en', $lang, false);
+      setcookie("lang", $this->UserLng, time()+2*24*60*60, "/");
+      $langFile = $this->path . $this->UserLng . $this->extention;
       if(!file_exists($langFile)){
-        throw new Execption("Language could not be loaded"); //or default to a language
+        throw new \Execption("Language could not be loaded");
       }
 
       $file = file_get_contents($langFile);
@@ -34,91 +38,67 @@ class Language {
   }
 
   private function detectLang($allowed_languages, $default_language, $lang_variable = null, $strict_mode = true) {
-      // $_SERVER['HTTP_ACCEPT_LANGUAGE'] verwenden, wenn keine Sprachvariable mitgegeben wurde
-      if ($lang_variable === null) {
-        $lang_variable = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+    if ($lang_variable === null) {
+      $lang_variable = $_SERVER['HTTP_ACCEPT_LANGUAGE'];
+    }
+
+    if (empty($lang_variable)) {
+      return $default_language;
+    }
+
+    $accepted_languages = preg_split('/,\s*/', $lang_variable);
+
+    $current_lang = $default_language;
+    $current_q = 0;
+
+    foreach ($accepted_languages as $accepted_language) {
+      $res = preg_match (
+        '/^([a-z]{1,8}(?:-[a-z]{1,8})*)(?:;\s*q=(0(?:\.[0-9]{1,3})?|1(?:\.0{1,3})?))?$/i',
+        $accepted_language,
+        $matches
+      );
+
+      if (!$res) {
+        continue;
+      }
+      $lang_code = explode ('-', $matches[1]);
+
+      if (isset($matches[2])) {
+        $lang_quality = (float)$matches[2];
+      } else {
+        $lang_quality = 1.0;
       }
 
-      // wurde irgendwelche Information mitgeschickt?
-      if (empty($lang_variable)) {
-        // Nein? => Standardsprache zurückgeben
-        return $default_language;
-      }
-
-      // Den Header auftrennen
-      $accepted_languages = preg_split('/,\s*/', $lang_variable);
-
-      // Die Standardwerte einstellen
-      $current_lang = $default_language;
-      $current_q = 0;
-
-      // Nun alle mitgegebenen Sprachen abarbeiten
-      foreach ($accepted_languages as $accepted_language=>$accepted_language2) {
-        // Alle Infos über diese Sprache rausholen
-        $res = preg_match (
-          '/^([a-z]{1,8}(?:-[a-z]{1,8})*)(?:;\s*q=(0(?:\.[0-9]{1,3})?|1(?:\.0{1,3})?))?$/i',
-          $accepted_language,
-          $matches
-        );
-
-        // war die Syntax gültig?
-        if (!$res) {
-          // Nein? Dann ignorieren
-          continue;
-        }
-
-        // Sprachcode holen und dann sofort in die Einzelteile trennen
-        $lang_code = explode ('-', $matches[1]);
-
-        // Wurde eine Qualität mitgegeben?
-        if (isset($matches[2])) {
-          // die Qualität benutzen
-          $lang_quality = (float)$matches[2];
-        } else {
-          // Kompabilitätsmodus: Qualität 1 annehmen
-          $lang_quality = 1.0;
-        }
-
-        // Bis der Sprachcode leer ist...
-        while (count ($lang_code)) {
-          // mal sehen, ob der Sprachcode angeboten wird
-          if (in_array (strtolower (join ('-', $lang_code)), $allowed_languages)) {
-            // Qualität anschauen
-            if ($lang_quality > $current_q) {
-              // diese Sprache verwenden
-              $current_lang = strtolower (join ('-', $lang_code));
-              $current_q = $lang_quality;
-              // Hier die innere while-Schleife verlassen
-              break;
-            }
-          }
-          // Wenn wir im strengen Modus sind, die Sprache nicht versuchen zu minimalisieren
-          if ($strict_mode) {
-            // innere While-Schleife aufbrechen
+      while (count ($lang_code)) {
+        if (in_array (strtolower (join ('-', $lang_code)), $allowed_languages)) {
+          if ($lang_quality > $current_q) {
+            $current_lang = strtolower (join ('-', $lang_code));
+            $current_q = $lang_quality;
             break;
           }
-          // den rechtesten Teil des Sprachcodes abschneiden
-          array_pop ($lang_code);
         }
+        if ($strict_mode) {
+          break;
+        }
+        array_pop ($lang_code);
       }
-
-      // die gefundene Sprache zurückgeben
-      return $current_lang;
+    }
+    return $current_lang;
   }
 
 
   public function getExistingLangs(){
-    $path    = '../langs/';
-    $files = scandir($path);
-    $files = array_diff(scandir($path), array('.', '..'));
-    $this->langs = str_replace(".json", "", $files);
+    $files = scandir($this->path);
+    $files = array_diff($files, array('.', '..'));
+    $files = array_values($files);
+    $this->langs = str_replace($this->extention, "", $files);
     return $this->langs;
   }
 
   public function getSpecificLangInfo($l){
-    $langFile = '../langs/'. $l . '.json';
+    $langFile = $this->path . $l . $this->extention;
     if(!file_exists($langFile)){
-      throw new Execption("Language could not be found");
+      throw new \Execption("Language could not be found");
     }
 
     $file = file_get_contents($langFile);
@@ -129,7 +109,6 @@ class Language {
   public function getLangInfo(){
     return $this->langinfo;
   }
-
 
   public function userLanguage(){
       return $this->lang;
